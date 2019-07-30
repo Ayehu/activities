63f8ad1cd7506062352e05390c006fefba3a9423
yaronly <44620860+yaronly@users.noreply.github.com>
2019-07-30 14:46:42 -0400

    Add files via upload

---
+++ Math/Truncate.vb
@@ -0,0 +1,33 @@
+Imports Ayehu.Sdk.ActivityCreation.Interfaces
+Imports Ayehu.Sdk.ActivityCreation.Extension
+Imports System.Text
+Imports System
+Imports System.Xml
+Imports System.Data
+Imports System.IO
+Imports System.Collections.Generic
+Imports Microsoft.VisualBasic
+
+Namespace Ayehu.Sdk.ActivityCreation
+    Public Class ActivityClass
+        Implements IActivity
+
+
+        Public TheValue As String
+
+        Public Function Execute() As ICustomActivityResult Implements IActivity.Execute
+
+
+            Dim dt As DataTable = New DataTable("resultSet")
+            dt.Columns.Add("Result", GetType(String))
+
+
+            dt.Rows.Add(Math.Truncate(Convert.ToDecimal(TheValue)))
+
+            Return Me.GenerateActivityResult(dt)
+        End Function
+
+
+    End Class
+End Namespace
+
